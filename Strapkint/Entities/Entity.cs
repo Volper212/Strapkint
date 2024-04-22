6aadacd11a59d1dabc4afd44c8e21dce9fd85423
Igor Święs <47470991+Volper212@users.noreply.github.com>
2024-04-22 18:38:17 +0200

    Delete commented code

--- Strapkint/Entities/Entity.cs
+++ Strapkint/Entities/Entity.cs
@@ -28,56 +28,5 @@ namespace Strapkint.Entities
         }
 
         public override Position ViewportPosition => new Position(Position.X, Level.Height - Position.Y - Height) - Viewport.Position;
-
-        //public Entity(char[,] text, Position position, Level level)
-        //    : this(text, position, Corner.BottomLeft)
-        //{
-        //    IsFixed = false;
-        //    _level = level;
-        //    Level.CurrentEntities.Add(this);
-        //}
-
-        //public Entity(char[,] text, Position position, Corner corner = Corner.TopLeft)
-        //    : base(text.GetLength(0), text.GetLength(1))
-        //{
-        //    Text = text;
-        //    Position = position;
-        //    DarkColorMap = new ConsoleColor?[Width, Height];
-        //    LightColorMap = new ConsoleColor?[Width, Height];
-        //    Corner = corner;
-        //}
-
-        //public char[,] Text { get; }
-        //public virtual Position Position { get; }
-        //public ConsoleColor?[,] DarkColorMap { get; }
-        //public ConsoleColor?[,] LightColorMap { get; }
-        //public bool IsFixed { get; } = true;
-        //public Corner Corner { get; }
-
-
-        //public char this[int x, int y]
-        //{
-        //    get => Text[x, y];
-        //    set => Text[x, y] = value;
-        //}
-
-        //public Position GetPositionInside(Viewport viewport)
-        //{
-        //    Rectangle referenceRectangle = IsFixed ? (Rectangle)viewport : Level;
-        //    var output = new Position(Position);
-        //    if (Corner.HasFlag(Corner.Bottom))
-        //    {
-        //        output.Y = referenceRectangle.Height - Position.Y - Height;
-        //    }
-        //    if (Corner.HasFlag(Corner.Right))
-        //    {
-        //        output.X = referenceRectangle.Width - Position.X - Width;
-        //    }
-        //    if (!IsFixed)
-        //    {
-        //        output -= viewport.Position;
-        //    }
-        //    return output;
-        //}
     }
 }
